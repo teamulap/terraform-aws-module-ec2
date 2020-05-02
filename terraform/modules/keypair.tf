2c0d69b872f7d572ad55d0012d20d979de731c8c
jcbagtas <17738997+jcbagtas@users.noreply.github.com>
2020-05-02 18:15:40 +0800

    Updated Syntax

--- terraform/modules/keypair.tf
+++ terraform/modules/keypair.tf
@@ -1,5 +1,5 @@
 resource "aws_key_pair" "default" {
-    count      = "${var.ssh_public_key!="" ? var.nb_instances : 0}"
-    key_name   = "${var.ssh_key_name}-${var.instance_set_name}-${count.index}"
-    public_key = "${var.ssh_public_key}"
+  count      = var.ssh_public_key != "" ? var.nb_instances : 0
+  key_name   = join("-",[var.ssh_key_name, var.instance_set_name, count.index])
+  public_key = var.ssh_public_key
 }
--- terratest/examples/aws-ec2-example/main.tf
+++ terratest/examples/aws-ec2-example/main.tf
@@ -1,24 +1,24 @@
 module "ec2" {
-    source              = "../../../terraform/modules"
-    region              = "ap-northeast-1"
-    subnet_id           = "subnet-949ec8dd"
-    security_group_ids  = ["sg-2764355e"]
-    ami_id              = "ami-0f310fced6141e627"
-    nb_instances        = "1"
-    instance_type       = "t3.nano"
-    instance_set_name   = "terratest-ec2"
-    associate_public_ip_address = "true"
-    ssh_key_name        = "terratest-my-key"
-    ssh_public_key      = "${file("../../test/id_rsa.pub")}"
-    instance_tags       = {
-                            name = "Terratest-EC2-1"
-                            project = "Terratest"
-                            environment = "development"
-                            contact = "jcbagtas"
-                            temporary = "true"
-                            costCenter = "JC"
-                            terraform = "true"
-                            managedBy = "jcbagtas"
-                        }
-    user_data = "${file("../../test/user_data.sh")}"
+  source                      = "../../../terraform/modules"
+  region                      = "ap-northeast-1"
+  subnet_id                   = "subnet-949ec8dd"
+  security_group_ids          = ["sg-2764355e"]
+  ami_id                      = "ami-0f310fced6141e627"
+  nb_instances                = "1"
+  instance_type               = "t3.nano"
+  instance_set_name           = "terratest-ec2"
+  associate_public_ip_address = "true"
+  ssh_key_name                = "terratest-my-key"
+  ssh_public_key              = file("../../test/id_rsa.pub")
+  instance_tags = {
+    name        = "Terratest-EC2-1"
+    project     = "Terratest"
+    environment = "development"
+    contact     = "jcbagtas"
+    temporary   = "true"
+    costCenter  = "JC"
+    terraform   = "true"
+    managedBy   = "jcbagtas"
+  }
+  user_data = file("../../test/user_data.sh")
 }
